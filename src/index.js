56f5745197334ae0ff2208c7801048f8f3342439
Here East <nastiapiven@gmail.com>
2021-03-11 19:30:16 +0100

    feat: update code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 
 module.exports = function towelSort (matrix) {
-  if (!matrix) {
+  if (matrix == undefined || matrix == []) {
     return [];
   }
-  
+
   let newArr = [];
   for (let i = 0; i < matrix.length; i++){
+    if (i%2 !== 0) {
+      matrix[i] = matrix[i].reverse();
+    }
      newArr = newArr.concat(matrix[i]);
   }
   return newArr;
